77d4132ef3ba9816c942fd49a37cb894fb7f6e6d
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2016-11-29 17:24:26 -0500

    Consulta de guia

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,7 +1,8 @@
 class GuidesController < ApplicationController
   def index
     if params[:q]
-      @guides=Guide.where(radicado: params[:q])      
+      @guides=Guide.where("lower(radicado) like :q", q: "%#{params[:q].downcase}%")     
+       
     else
       @guides=Guide.none
     end
